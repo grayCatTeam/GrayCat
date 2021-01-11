e4e559283058c89b8c97b4234b1f6c7c52772891
Mehmet Arda Çelik <46358038+ardaninsaturnu@users.noreply.github.com>
2021-01-11 21:30:09 +0300

    form page edited all form box

--- src/Pages/Form/index.js
+++ src/Pages/Form/index.js
@@ -28,17 +28,17 @@ function FormPage() {
         }}
       >
         <Form>
-          <div className={`d-flex flex-column ${classes.formBox}`}>
+          <div className={`container mx-auto px-5 d-flex flex-column g-0 ${classes.formBox}`}>
             <FormLabel label="Upload a Photo"/>
             <Field id="photo" name="photo" placeholder="jpeg or png" type="file"/>
           </div>
 
-          <div className={`d-flex flex-column ${classes.formBox}`}>
+          <div className={`container mx-auto px-5 d-flex flex-column g-0 ${classes.formBox}`}>
             <FormLabel label="fullname"/>
             <Field id="fullname" name="fullname" placeholder="John Doe"/>
           </div>
 
-          <div className={`d-flex flex-column ${classes.formBox}`}>
+          <div className={`container mx-auto px-5 d-flex flex-column g-0 ${classes.formBox}`}>
             <FormLabel label="e mail"/>
             <Field
               id="email"
@@ -48,7 +48,7 @@ function FormPage() {
             />
           </div>
 
-          <div className={`d-flex flex-column ${classes.formBox}`}>
+          <div className={`container mx-auto px-5 d-flex flex-column g-0 ${classes.formBox}`}>
             <FormLabel label="phone"/>
             <Field
               id="phone"
@@ -58,7 +58,7 @@ function FormPage() {
             />
           </div>
 
-          <div className={`d-flex flex-column ${classes.formBox}`}>
+          <div className={`container mx-auto px-5 d-flex flex-column g-0 ${classes.formBox}`}>
             <FormLabel label="city"/>
             <Field
               id="city"
@@ -67,46 +67,50 @@ function FormPage() {
               type="select"
             />
           </div>
-          <div className={`${classes.formBox} row d-flex`}>
-            <div className="col-md-4">
-              <div className={`d-flex flex-column ${classes.formBoxMini}`}>
+          <div className={`container mx-auto px-5 d-flex flex-column g-0 ${classes.formBox}`}>
+            <div className={`row`}>
+              <div className="col-xl-4">
+                <div className={`w-100 d-flex flex-column`}>
 
-                <FormLabel label="height (cm)"/>
-                <Field
-                  id="height"
-                  name="height"
-                  placeholder="180"
-                  type="number"
-                />
+                  <FormLabel label="height (cm)"/>
+                  <Field
+                    id="height"
+                    name="height"
+                    placeholder="180"
+                    type="number"
+                  />
+                </div>
+              </div>
+              <div className="col-xl-4">
+                <div className={`w-100 d-flex flex-column`}>
+
+                  <FormLabel label="weight (cm)"/>
+                  <Field
+                    id="weight"
+                    name="weight"
+                    placeholder="60"
+                    type="number"
+                  />
+                </div>
+              </div>
+              <div className="col-xl-4">
+                <div className={`w-100 d-flex flex-column`}>
+                  <FormLabel label="shoe"/>
+                  <Field
+                    id="shoe"
+                    name="shoe"
+                    placeholder="38"
+                    type="number"
+                  />
+                </div>
               </div>
             </div>
-            <div className="col-md-4">
-              <div className={`d-flex flex-column ${classes.formBoxMini}`}>
 
-                <FormLabel label="weight (cm)"/>
-                <Field
-                  id="weight"
-                  name="weight"
-                  placeholder="60"
-                  type="number"
-                />
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className={`d-flex flex-column ${classes.formBoxMini}`}>
 
-                <FormLabel label="shoe"/>
-                <Field
-                  id="shoe"
-                  name="shoe"
-                  placeholder="38"
-                  type="number"
-                />
-              </div>
-            </div>
+
           </div>
 
-          <div className={`d-flex flex-column ${classes.formBox}`}>
+          <div className={`container mx-auto px-5 d-flex flex-column g-0 ${classes.formBox}`}>
 
             <FormLabel label="skills"/>
             <Field
@@ -119,9 +123,9 @@ function FormPage() {
 
           {/*--language level area--*/}
 
-          <div className={`${classes.formBox} row d-flex justify-content-center align-items-center`}>
-            <div className="col-md-5">
-              <div className={`d-flex flex-column ${classes.formBoxMini}`}>
+          <div className={`container mx-auto px-5 d-flex flex-column g-0 ${classes.formBox}`}>
+            <div className="row">
+              <div className={`col-xl-5 d-flex flex-column`}>
 
                 <FormLabel label="language"/>
                 <Field
@@ -131,9 +135,7 @@ function FormPage() {
                   type="text"
                 />
               </div>
-            </div>
-            <div className="col-md-5">
-              <div className={`d-flex flex-column ${classes.formBoxMini}`}>
+              <div className={`col-xl-5 d-flex flex-column`}>
 
                 <FormLabel label="level"/>
                 <Field
@@ -143,104 +145,100 @@ function FormPage() {
                   type="email"
                 />
               </div>
-            </div>
-            <div className="col-md-2">
-              <div className={`d-flex flex-column justify-content-end ${classes.formBoxMini}`}>
-                <button className={classes.plusButton}><FaPlus/></button>
-
+              <div className="col-xl-2 d-flex justify-content-center align-items-end">
+                <button className={`bg-transparent rounded-circle text-center py-2 px-3 fs-3 ${classes.addButton}`}><FaPlus/></button>
               </div>
             </div>
+
           </div>
 
           {/*--education area--*/}
 
-          <div className={`${classes.formBox} row d-flex`}>
-            <div className="col-md-12">
-              <div className={`d-flex flex-column`}>
-
-                <FormLabel label="education"/>
-                <Field
-                  id="school"
-                  name="school"
-                  placeholder="John Hopkins University"
-                  type="text"
-                />
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className={`d-flex flex-column ${classes.formBox}`}>
-
-                <FormLabel label="degree"/>
-                <Field
-                  id="degree"
-                  name="degree"
-                  placeholder="Bachelor"
-                  type="number"
-                />
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className={`d-flex flex-column ${classes.formBox}`}>
-
-                <FormLabel label="profession"/>
-                <Field
-                  id="profession"
-                  name="profession"
-                  placeholder="Art History"
-                  type="text"
-                />
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className={`d-flex flex-column ${classes.formBox}`}>
-
-                <FormLabel label="graduation"/>
-                <Field
-                  id="graduation"
-                  name="graduation"
-                  placeholder="2024"
-                  type="date"
-                />
-              </div>
-            </div>
-            <div className="col-md-12">
-              <button className={classes.addEdu}><FaPlus/></button>
-            </div>
-          </div>
-
-          <div className={`d-flex flex-column ${classes.formBox}`}>
-
-            <FormLabel label="instagram"/>
+          <div className={`container mx-auto px-5 d-flex flex-column g-0 ${classes.formBox}`}>
+            <FormLabel label="education"/>
             <Field
-              id="instagram"
-              name="instagram"
-              placeholder="username"
-              type="url"
-            />
-          </div>
-          <div className={`d-flex flex-column ${classes.formBox}`}>
-
-            <FormLabel label="twitter"/>
-            <Field
-              id="twitter"
-              name="twitter"
-              placeholder="username"
+              id="school"
+              name="school"
+              placeholder="John Hopkins University"
               type="text"
             />
-          </div>
-          <div className={`d-flex flex-column ${classes.formBox}`}>
+            <div className={`row`}>
 
-            <FormLabel label="vimeo"/>
-            <Field
-              id="vimeo"
-              name="vimeo"
-              placeholder="username"
-              type="text"
-            />
+              <div className="col-xl-4">
+                <div className={`w-100 d-flex flex-column`}>
+                  <FormLabel label="degree"/>
+                  <Field
+                    id="degree"
+                    name="degree"
+                    placeholder="Bachelor"
+                    type="number"
+                  />
+                </div>
+
+              </div>
+              <div className="col-md-4">
+                <div className={`w-100 d-flex flex-column`}>
+
+                  <FormLabel label="profession"/>
+                  <Field
+                    id="profession"
+                    name="profession"
+                    placeholder="Art History"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div className="col-md-4">
+                <div className={`w-100 d-flex flex-column`}>
+
+                  <FormLabel label="graduation"/>
+                  <Field
+                    id="graduation"
+                    name="graduation"
+                    placeholder="2024"
+                    type="date"
+                  />
+                </div>
+              </div>
+              <div className="col-md-12">
+                <button className={`w-100 bg-transparent py-3 mt-4 fs-4 ${classes.addButton}`}><FaPlus/></button>
+              </div>
+            </div>
           </div>
 
-          <div className={classes.formBox}>
-            <button type="submit" className={classes.button}>SUBMIT</button>
+          {/*social media*/}
+          <div className={`container mx-auto px-5 d-flex flex-column g-0 ${classes.formBox}`}>
+            <div className={`d-flex flex-column`}>
+              <FormLabel label="instagram"/>
+              <Field
+                id="instagram"
+                name="instagram"
+                placeholder="username"
+                type="url"
+              />
+            </div>
+            <div className={`d-flex flex-column`}>
+              <FormLabel label="twitter"/>
+              <Field
+                id="twitter"
+                name="twitter"
+                placeholder="username"
+                type="text"
+              />
+            </div>
+            <div className={`d-flex flex-column`}>
+              <FormLabel label="vimeo"/>
+              <Field
+                id="vimeo"
+                name="vimeo"
+                placeholder="username"
+                type="text"
+              />
+            </div>
+          </div>
+
+          <div className={`container mx-auto px-5 d-flex flex-column g-0`}>
+            <button className={`w-100 py-3 mt-4 fs-4 fw-bold bg-transparent my-5 ${classes.addButton}`} type="submit">SUBMIT</button>
           </div>
 
         </Form>
