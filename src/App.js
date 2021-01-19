68d28e5ac51d527324d0c2415231c79ce80e7efc
Mehmet Arda Çelik <46358038+ardaninsaturnu@users.noreply.github.com>
2021-01-19 12:05:01 +0300

    new homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import About from './Pages/About';
 import Home from './Pages/Home';
-import OurWorks from './Pages/OurWorks';
-import Casting from './Pages/Casting';
-import CastSingle from './Pages/CastSingle';
-import ProductPage from './Pages/ProductPage';
-import Contact from './Pages/Contact';
+
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/contact" component={Contact} />
-          <Route path="/product" component={ProductPage} />
-          <Route path="/cast-profile" component={CastSingle} />
-          <Route path="/casting" component={Casting} />
-          <Route path="/works" component={OurWorks} />
-          <Route path="/about" component={About} />
           <Route path="/" component={Home} />
         </Switch>
       </Router>
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,28 +1,47 @@
-import Header from '../../Components/Header';
-import home from '../../Assets/video/home.mp4';
-import Title from '../../Atoms/Title'
-import classes from './home.module.css';
-import rainlogo from "../../Assets/images/rain_media_logo-01.svg";
+import {Link} from "react-router-dom";
 
+import classes from './home.module.css'
+
+
+import NavMenu from "../../Atoms/NavMenu";
+
+import logo from '../../Assets/images/rain_media_logo-01.svg'
+import {FaFacebookF, FaTwitter, FaVimeo} from "react-icons/fa";
 
 function Home() {
   return (
-    <div className="container-fluid g-0 vh-100">
-      <div className="row g-0 position-relative">
 
-        <div className={classes.overlay}>
-          <Header />
-          <div className={`d-flex justify-content-center align-items-center flex-column ${classes.logoWraper}`}>
-            <img src={rainlogo} width={600}/>
-            <Title frontSide="We Create Future With Rain" backSide="We Create Future With Rain"/>
+      <div className={`container-fluid bg-dark vh-100`}>
+
+
+        <div className={`row`}>
+          <div className={`col-xl-8 bg-white p-5`}>
+            <NavMenu/>
+          </div>
+          <div className={`col-xl-4 bg-warning text-center d-flex justify-content-center flex-column align-items-center`}>
+            <img src={logo} width={300}/>
+            <div className={`row g-0 d-flex w-50`}>
+              <div className={`col-xl-5 d-flex flex-column g-0 align-items-end`}>
+                <a>TR</a>
+                <a>EN</a>
+              </div>
+              <div className={`col-xl-1 d-flex justify-content-center`}>
+                <div className={`h-100 bg-dark ${classes.line}`}></div>
+              </div>
+              <div className={`col-xl-5 d-flex flex-column g-0 align-items-start`}>
+                <a><FaTwitter/></a>
+                <a><FaFacebookF/></a>
+                <a><FaVimeo/></a>
+              </div>
+
+            </div>
           </div>
 
         </div>
-        <video autoPlay loop muted className={classes.video}>
-          <source src={home} type="video/mp4" />
-        </video>
+
+
+
       </div>
-    </div>
   );
 }
 
