442861a23d20f533fc4dc8632b72c727aeca2c44
Mehmet Arda Çelik <46358038+ardaninsaturnu@users.noreply.github.com>
2021-02-25 08:57:47 +0300

    hero section done

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,6 +1,15 @@
 import React from "react"
 
 
+import classes from '../style/hero.module.css'
+
+import dayi from '../images/dayi.png'
+import rose from '../images/rose.png'
+import medusa from '../images/medusa.png'
+import points from '../images/points.png'
+import sun from '../images/sun.png'
+
+
 
 
 
@@ -9,23 +18,31 @@ const Hero = () => {
 
   return (
 
-    <div className={"row vh-100 bg-light"}>
+    <div className={`container ${classes.hero}`}>
+      <div className={"row"}>
 
-      <div className={`col-xl-6`}>
-        <p className={`text-dark`}>
-          We give shape to brand
-          identities and design digital
-          experiences to help
-          organizations innovate their
-          business<span className={"text-danger"}>.</span>
-        </p>
-      </div>
-      <div className={`col-xl-6 bg-dark`}>
-
-        <div>
-
+        <div className={`col-xl-6`}>
+          <p className={`text-light ${classes.heroText}`}>
+            We give shape to brand
+            identities and design digital
+            experiences to help
+            organizations innovate their
+            business<span className={"text-danger"}>.</span>
+          </p>
         </div>
 
+        <div className={`col-xl-6`}>
+
+          <div className={`${classes.imageWrapper} position-relative`}>
+            <img className={`position-absolute ${classes.medusa}`} src={medusa} width="330"/>
+            <img className={`position-absolute ${classes.dayi}`} src={dayi}/>
+            <img className={`position-absolute ${classes.rose}`} src={rose}/>
+            <img className={`position-absolute ${classes.sun}`} src={sun}/>
+            <img className={`position-absolute ${classes.points}`} src={points}/>
+          </div>
+
+
+        </div>
 
       </div>
 
