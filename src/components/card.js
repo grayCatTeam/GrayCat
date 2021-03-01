418525f3260875069e16eca65f3bcc4fb7cdba43
Mehmet Arda Çelik <46358038+ardaninsaturnu@users.noreply.github.com>
2021-03-01 16:10:02 +0300

    card design

--- src/components/card.js
+++ src/components/card.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef } from "react"
+import gsap from 'gsap/all'
 import classes from '../style/card.module.css'
 
 
@@ -6,11 +7,20 @@ import classes from '../style/card.module.css'
 const Card = (props) => {
 
 
+  let cardComponent = useRef(null)
+
+
+  useEffect(() => {
+
+
+
+  },[])
+
 
 
 return (
 
-<div className={`card w-sm-100 ${classes.cardStyle}`}>
+<div ref={el => ( cardComponent = el)} className={`card w-sm-100 ${classes.cardStyle}`}>
 
   <img src={props.image} className={`${classes.image}`} alt="image" width={props.width}/>
   <div className="card-title fs-2">
