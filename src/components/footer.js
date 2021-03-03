6026bc52fa067e1ee94e70b3398effcf0c7f6377
Mehmet Arda Çelik <46358038+ardaninsaturnu@users.noreply.github.com>
2021-03-03 10:48:11 +0300

    footer responsive

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,19 +12,19 @@ const Footer = () => {
     <footer style={{minHeight:'300px'}}>
       <div className={`container`}>
 
-        <div className={`row py-5`}>
+        <div className={`row py-5 d-flex flex-wrap`}>
           <div className={`col-xl-3  ${classes.contact}`}>
-            <div className={` mb-4`}>
+            <div className={` mb-4 text-center`}>
               <img src={logo} width={"50%"}/>
             </div>
-            <div>
-              <p>Abbasaga / Besiktas</p>
-              <p>Istanbul</p>
-              <a className={'text-white'} href={'mailto:info@graycat.co'}>info@graycat.co</a>
+            <div className={`text-center`}>
+              <p className={`text-center`}>Abbasaga / Besiktas</p>
+              <p className={`text-center`}>Istanbul</p>
+              <a className={'text-white text-center'} href={'mailto:info@graycat.co'}>info@graycat.co</a>
             </div>
           </div>
 
-          <div className={`col-xl-3 ps-5`}>
+          <div className={`col-xl-3 ps-5  ${classes.column}`}>
             <h3 className={`fs-4`}>Social</h3>
             <ul>
               <li><a className={`text-white`} href={"https://www.linkedin.com/company/likeacat"} target={"_blank"}>Linkedin</a></li>
@@ -34,10 +34,10 @@ const Footer = () => {
             </ul>
           </div>
 
-          <div className={`col-xl-3 ps-5 md-my-4`}>
-            <div className={classes.title}>
+          <div className={`col-xl-3 ps-5 md-my-4 ${classes.column}`}>
+
               <h3 className={`fs-4`}>Partners</h3>
-            </div>
+
             <ul>
               <li><a className={`text-white`} href={"https://ads.google.com/aw/campaigns/new/express?campaignId=10580265964&ocid=530083575&subid=tr-tr-ha-aw-sk-c-bau%21o3~Cj0KCQiA4feBBhC9ARIsABp_nbVpNOdMWWxEsTdqs-JHdA0Y8f4zGPI0J1QoY9JU1FhA9sIKlsmJ094aAul0EALw_wcB~11226936979&step=crev&euid=422696844&__u=2735040556&uscid=530083575&__c=3681330175&authuser=0&sourceid=emp"} target={"_blank"}>Google</a></li>
               <li><a className={`text-white`} href={"https://www.shopify.com/"} target={"_blank"}>Shopify</a></li>
@@ -46,7 +46,7 @@ const Footer = () => {
             </ul>
           </div>
 
-          <div className={`col-xl-3 ps-5 md-my-4`}>
+          <div className={`col-xl-3 ps-5 md-my-4 ${classes.column}`}>
             <h3 className={`fs-4`}>Support</h3>
             <ul>
               <li><a className={`text-white`} href={"https://www.shopify.com/"} target={"_blank"}>FAQ</a></li>
