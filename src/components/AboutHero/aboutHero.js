a468562ecb736ae905920982e2c79c5339074ad6
ardaninsaturnu <hsmclk2505@gmail.com>
2021-04-06 13:10:08 +0300

    about hero

--- src/components/AboutHero/aboutHero.js
+++ src/components/AboutHero/aboutHero.js
@@ -6,16 +6,28 @@ import arrowDown from "../../assets/icons/arrowDown.svg"
 const AboutHero = () => {
 
   return (
+      <>
+        <div className={classes.aboutHero}>
 
+              <div className={classes.textHero}>
+                <h1>We Craft Experiences</h1>
+                <p>
+                  We are a remote design agency based in Istanbul,
+                  working with clients around the world. As
+                  passionate designers, we love building products
+                  that are easy to use, accessible, engaging, and delightful.
+                </p>
+              </div>
+          <div className="text-center">
+            <img className={classes.arrowDown} src={arrowDown} width={"20px"}/>
 
-      <div className="text-center">
-        <img className={classes.arrowDown} src={arrowDown} width={"20px"}/>
-      </div>
+          </div>
+          </div>
+
+      </>
 
 
   )
-
-
 }
 
 export default AboutHero
