2c74a664a15d1149bcf4e7d2fde7156309f34059
ardaninsaturnu <hsmclk2505@gmail.com>
2021-04-06 23:32:37 +0300

    slide 2

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,5 +7,6 @@
 // You can delete this file if you're not using it
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-bootstrap';
 import {gsap} from 'gsap';
 
--- src/atoms/AboutSlide/index.js
+++ src/atoms/AboutSlide/index.js
@@ -1,4 +1,12 @@
 import React from "react";
+import Carousel from 'react-bootstrap/Carousel';
+
+import wireframe from "../../assets/images/wireframe@2x.png";
+import analyze from "../../assets/images/analyze@2x.png";
+import coding from "../../assets/images/coding@2x.png";
+import motion from "../../assets/images/motion@2x.png"
+
+
 import classes from './aboutSlide.module.css';
 
 const AboutSlide = () => {
@@ -7,9 +15,39 @@ const AboutSlide = () => {
       <>
         <div className={classes.aboutSlide}>
           <div className={classes.slideBox}>
-
+            <Carousel interval={5000}>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={analyze}
+                  alt="First slide"
+                />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100 h-100"
+                  src= {wireframe}
+                  alt="Second slide"
+                />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100 h-100"
+                  src= {coding}
+                  alt="Second slide"
+                />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100 h-100"
+                  src= {motion}
+                  alt="Second slide"
+                />
+              </Carousel.Item>
+            </Carousel>
           </div>
         </div>
+
       </>
 
 
