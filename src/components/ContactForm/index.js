315394a55f178bdd6a50363b6a87f522d82a1468
ardaninsaturnu <hsmclk2505@gmail.com>
2021-04-14 15:14:40 +0300

    new photos for teamcard

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,11 +1,58 @@
 import React from 'react';
 
 import classes from './contactform.module.css';
+import { Formik,Form,Field } from "formik"
 
 const ContactForm = () => {
   return(
     <>
+      <Formik
+        initialValues={{
+          nameOrCompany: '',
+          email: '',
+          budget: '',
+          message: ''
+        }}
+      >
+        <Form>
+          <label htmlFor="nameOrCompany">name || company :</label>
+          <Field
+            name="nameOrCompany"
+            type="text"
+            placeHolder="Rain Medya"
+          />
 
+          <label htmlFor="email">email :</label>
+          <Field
+            name="email"
+            type="email"
+            placeHolder="info@rainmedia.org"
+          />
+
+          <label htmlFor="budget">select budget :</label>
+          <Field
+            name="budget"
+            type="radio"
+          />
+          <Field
+            name="budget"
+            type="radio"
+          />
+          <Field
+            name="budget"
+            type="radio"
+          />
+
+          <label htmlFor="message">Project Description :</label>
+          <Field
+            name="message"
+            type="textarea"
+            placeHolder="message"
+          />
+
+
+        </Form>
+      </Formik>
     </>
   )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,6 +4,7 @@ import Layout from "../components/layout"
 import ActionCard from "../atoms/ActionCard/actionCard"
 import Header from "../components/Header/header"
 import ContactHero from "../components/ContactHero"
+import ContactForm from "../components/ContactForm"
 
 
 
@@ -14,6 +15,7 @@ const Contact = () => {
     <>
     <Header/>
     <ContactHero/>
+    <ContactForm/>
     </>
 
 
